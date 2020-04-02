a71822c1d78a71af79e2575607b766bf9c14f8e9
Emily Thompson <ekthompson59@gmail.com>
2020-04-01 19:46:02 -0700

    Complete Chat App

--- Flash Chat iOS13/Controllers/RegisterViewController.swift
+++ Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class RegisterViewController: UIViewController {
 
@@ -14,6 +15,32 @@ class RegisterViewController: UIViewController {
     @IBOutlet weak var passwordTextfield: UITextField!
     
     @IBAction func registerPressed(_ sender: UIButton) {
+        
+        func createAlert(title: String, message: String) {
+            let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: {(action) in
+                alert.dismiss(animated: true, completion: nil)
+            }))
+                
+                self.present(alert, animated: true, completion: nil)
+        }
+        
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                if let e = error {
+                    createAlert(title: "Error", message: "Password must be at least 6 characters")
+                    print(e.localizedDescription)
+                    //localizedDescription uses users' phone language
+                } else {
+                    //Navigate to Chat View Controller
+                    self.performSegue(withIdentifier: K.registerSegue, sender: self)
+                }
+            }
+        }
+    
+    
+    
+    
     }
     
 }
